14a487489fceb702a11d85b2094aa0da06b52ca7
Justin Hoyt <justinhoyt@Justins-MacBook-Air.local>
2020-12-20 00:58:17 -0500

    add cors library back in

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,12 @@
+import * as cors from 'cors';
 import * as functions from 'firebase-functions';
+
+const corsHandler = cors({origin: true});
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 export const helloWorld = functions.https.onRequest((request, response) => {
-  response.set('Access-Control-Allow-Origin', '*');
-
-  if (request.method === 'OPTIONS') {
-    response.set('Access-Control-Allow-Methods', 'GET');
-    response.set('Access-Control-Allow-Headers', 'Content-Type');
-    response.set('Access-Control-Max-Age', '3600');
-    response.status(204).send('');
-  } else {
-    response.send('Hello World!');
-  }
-
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  corsHandler(request, response, () => {
+    response.send(`Ping from Firebase (with CORS handling)!`);
+  });
 });
