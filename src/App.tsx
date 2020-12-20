e2961adff1ed1d3d592076f1f84746732c7f7f5a
Justin Hoyt <justinhoyt@Justins-MacBook-Air.local>
2020-12-20 02:17:18 -0500

    Add firestore configuration

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,26 @@ import './App.css';
 import React, { useState } from 'react';
 import axios, { AxiosError, AxiosResponse } from 'axios';
 
+import { db } from './firebase';
 import logo from './logo.svg';
 
 function App() {
   const [displayText, setDisplayText] = useState('no data yet');
   axios.get('/helloWorld')
-    .then((response: AxiosResponse<string>) => {
-      console.log(`this is my response: ${response}`);
-      setDisplayText(response.data)
-    })
+    .then((response: AxiosResponse<string>) => setDisplayText(response.data))
     .catch((error: AxiosError<string>)  => console.log(`this is my error: ${error}`))
+
+  db.collection('room').doc('1').set({
+    users: ['1', '2'],
+    pot: "100",
+    board: ['K', 'A', '4'],
+  })
+  .then(function() {
+    console.log("Document successfully written!");
+  })
+  .catch(function(error) {
+      console.error("Error writing document: ", error);
+  });
 
   return (
     <div className="App">
---
+++ src/firebase.ts
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+let config = {
+    apiKey: "AIzaSyBMU4KJAhnsitFWy_WdYUw2MTQjjVxIApo",
+    authDomain: "simplepoker.firebaseapp.com",
+    databaseURL: "https://simplepoker-default-rtdb.firebaseio.com",
+    projectId: "simplepoker",
+    storageBucket: "simplepoker.appspot.com",
+    messagingSenderId: "349034050638",
+    appId: "1:349034050638:web:b8065e00239a36df67ff1f",
+    measurementId: "G-ZC9XMVKS13"
+};
+
+firebase.initializeApp(config);
+
+const db = firebase.firestore();
+
+export { db };
