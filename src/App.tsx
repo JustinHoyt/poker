63f6edbf3b01ac958332ca2be19e29cbafc97dbf
Justin Hoyt <justinhoyt@Justins-MacBook-Air.local>
2020-12-20 21:26:56 -0500

    Update database schema

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,24 @@ function App() {
     .catch((error: AxiosError<string>)  => console.log(`this is my error: ${error}`))
 
   db.collection('rooms').doc('1').set({
-    users: ['1', '2'],
+    users: [{
+      name: 'nick',
+      stack: 100,
+      action: '30',
+      cards: ['10s', '8d'],
+      seat: 1,
+    }, {
+      name: 'justin',
+      stack: 50,
+      action: 'fold',
+      cards: ['Ks', 'Qd'],
+      seat: 0,
+    }],
     pot: "100",
     board: ['Ks', 'As', '4c'],
+    button: 0,
+    blindSize: .02,
+    buyIn: 10
   })
   .then(function() {
     console.log("Document successfully written!");
